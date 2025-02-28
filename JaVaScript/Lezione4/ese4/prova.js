5885d3395b172a3129f870cfbd7fa2a96af14e27
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2025-02-28 21:52:09 +0100

    g

--- JaVaScript/Lezione4/ese4/prova.js
+++ JaVaScript/Lezione4/ese4/prova.js
@@ -5,8 +5,8 @@ let contenitore = document.createElement("div");
 contenitore.setAttribute("class", "container");
 
 // creazuione righa
-let righa11 = document.createElement("div");
-righa11.setAttribute("class", "row")
+let righa1 = document.createElement("div");
+righa1.setAttribute("class", "row")
 
 // creazione collone
 let colonna1 = document.createElement("div");
@@ -18,15 +18,17 @@ colonna2.setAttribute("class", "col");
 let colonna3 = document.createElement("div");
 colonna3.setAttribute("class", "col");
 
-righa11.appendChild(colonna1);
-righa11.appendChild(colonna2);
-righa11.appendChild(colonna3);
+let colonna4 = document.createElement("div");
+colonna4.setAttribute("class", "col");
 
-contenitore.appendChild(righa11);
+righa1.appendChild(colonna1);
+righa1.appendChild(colonna2);
+righa1.appendChild(colonna3);
+righa1.appendChild(colonna4);
 
+contenitore.appendChild(righa1);
 
 document.body.appendChild(contenitore);
-
 
 // ESE 1 - Titolo
 let box = document.createElement("h1");
@@ -39,10 +41,52 @@ box.style.border = "3px solid black";
 box.style.padding = "10px";
 box.style.margin = "10px";
 
-righa11.appendChild(box); // inserisco il box alla colonna
+colonna1.appendChild(box); // inserisco il box alla colonna
 
 document.getElementById("esercizio").innerText = "ESERCIZIO 1";
 
+// ESE 2 - Titolo nella seconda colonna
+let box2 = document.createElement("h1");
+box2.setAttribute("id", "esercizio2");
+box2.style.color = "green";
+box2.style.accentColor = "red";
+box2.style.backgroundColor = "yellow";
+box2.style.width = "300px";
+box2.style.border = "3px solid black";
+box2.style.padding = "10px";
+box2.style.margin = "10px";
+
+colonna2.appendChild(box2); // inserisco il box alla seconda colonna
+document.getElementById("esercizio2").innerText = "ESERCIZIO 2";
+
+// ESE 3 - Titolo della terza colonna
+let box3 = document.createElement("h1");
+box3.setAttribute("id", "esercizio3");
+box3.style.color = "green";
+box3.style.accentColor = "red";
+box3.style.backgroundColor = "yellow";
+box3.style.width = "300px";
+box3.style.border = "3px solid black";
+box3.style.padding = "10px";
+box3.style.margin = "10px";
+
+colonna3.appendChild(box3); // inserisco il box alla seconda colonna
+document.getElementById("esercizio3").innerText = "ESERCIZIO 3";
+
+// ESE 4 - Titolo della terza colonna
+let box4 = document.createElement("h1");
+box4.setAttribute("id", "esercizio4");
+box4.style.color = "green";
+box4.style.accentColor = "red";
+box4.style.backgroundColor = "yellow";
+box4.style.width = "300px";
+box4.style.border = "3px solid black";
+box4.style.padding = "10px";
+box4.style.margin = "10px";
+
+colonna4.appendChild(box4); // inserisco il box alla seconda colonna
+document.getElementById("esercizio4").innerText = "ESERCIZIO 4";
+/*
 // Tabellina con ciclo FOR
 let tabellina = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
 let magicNumber = 4;
@@ -62,7 +106,7 @@ box2CicloFor.style.margin = "10px";
 box2CicloFor.innerHTML = `<strong>Tabellina con ciclo For:</strong><br>` + risCicloFor;
 box2CicloFor.style.border = "3px solid black";
 
-//righa1.appendChild(box2CicloFor);
+colonna2.appendChild(box2CicloFor);
 
 // Tabellina con ciclo WHILE
 let valoreMax = 10;
@@ -84,23 +128,10 @@ box3CicloWhile.style.padding = "10px";
 box3CicloWhile.style.margin = "10px";
 box3CicloWhile.innerHTML = `<strong>Tabellina con ciclo WHILE:</strong><br>` + risCicloWhile;
 
-//righa11.appendChild(box3CicloWhile);
+righa1.appendChild(box3CicloWhile);*/
 
 
-// ESE 2 - Titolo nella seconda colonna
-let box2 = document.createElement("h1");
-box2.setAttribute("id", "esercizio2");
-box2.style.color = "green";
-box2.style.accentColor = "red";
-box2.style.backgroundColor = "yellow";
-box2.style.width = "300px";
-box2.style.border = "3px solid black";
-box2.style.padding = "10px";
-box2.style.margin = "10px";
 
-
-righa11.appendChild(box2); // inserisco il box alla seconda colonna
-document.getElementById("esercizio2").innerText = "ESERCIZIO 2";
 
 
 
