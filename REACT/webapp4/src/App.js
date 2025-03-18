2abee7a64fb0d7891d7c7fdfe17bb33dc85bf41e
Nicolas77777777 <sairwater@gmail.com>
2025-03-18 18:36:18 +0100

    seventh commit hp

--- REACT/webapp4/src/App.js
+++ REACT/webapp4/src/App.js
@@ -1,8 +1,36 @@
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link,} from "react-router-dom";
+
 import './App.css';
+import Contatti from './Contatti';
+import Su_di_me from './Su_di_me';
 
 function App() {
   return (
+
+
+    <BrowserRouter>
+
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">Chi Siamo</Link></li>
+          <li><Link to="/contact">Contatti</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+          <Route path='/about' Component={Su_di_me}></Route>
+          <Route path='/contact' Component={Contatti}></Route>
+      </Routes>
+
+    </BrowserRouter>
+
+
+
+  
+
+  
 
 
     
---
+++ REACT/webapp4/src/Su_di_me.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Su_di_me = () => {
+  return (
+    <div>Su_di_me</div>
+  )
+}
+
+export default Su_di_me
