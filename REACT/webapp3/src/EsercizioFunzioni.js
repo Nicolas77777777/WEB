bd86eca7d4ee78cc4861dc1ebd8fce436290086d
Nicolas77777777 <sairwater@gmail.com>
2025-03-18 12:56:27 +0100

    fiveth commit hp

--- REACT/webapp3/src/EsercizioFunzioni.js
+++ REACT/webapp3/src/EsercizioFunzioni.js
@@ -1,33 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const EsercizioFunzioni = () => {
     // Area1 
 
 
 
-    let salutare =""
+    const[salutare,setSalutare] =useState("");
+    const[somma,setSomma] = useState(0);
+
     let saluto = () => {
 
         let nome1 = document.getElementById("nome1").value;
+        let nome2 = document.getElementById("nome2").value;
+
         console.log(nome1+" ciao")
 
-        let salutare = nome1 + "ciao "
-
-        return salutare
+        setSalutare(nome1 + " ciao, piacere, io sono " + nome2)
 
 
-    }
+    };
+
+    let saluto2 = (a,b) => {
+
+      console.log(setSomma( a+b));
 
 
 
+  };
+
+  saluto2(3,5);
 
 
-    
+  
   return (
 
     <div>
         <h1>EsercizioFunzioni 5.1</h1>
+        Scrivi il tuo nome<input type="text" id="nome2"/><br/>
         Scrivi il tuo nome<input type="text" id="nome1"/><br/>
+
         <button onClick={saluto}>Saluta'</button>
         <h5>{salutare}</h5>
 
