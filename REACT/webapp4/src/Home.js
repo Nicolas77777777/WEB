87ef74bad2f8671dff4544fe345a0dec5e4bdbc6
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2025-03-19 02:24:29 +0100

    last commit Mac

--- REACT/webapp4/src/Home.js
+++ REACT/webapp4/src/Home.js
@@ -4,19 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
   return (
-    <div className="bg-home d-flex justify-content-center align-items-center text-white" style={{
+    <div className="bg-home d-flex flex-column justify-content-center align-items-center text-white" style={{
       backgroundImage: "url('/img/aquila2.jpg')",
-
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       height: '100vh'
     }}>
+      <div className="position-absolute top-0 w-100 d-flex justify-content-center p-3">
+        <Menu />
+      </div>
+
       <div className="text-center">
         <h1>Benvenuti nella Home</h1>
-        <Menu />
       </div>
     </div>
   );
 };
 
 export default Home;
+
--- REACT/webapp4/src/Su_di_me.js
+++ REACT/webapp4/src/Su_di_me.js
@@ -2,10 +2,31 @@ import React from 'react'
 
 const Su_di_me = () => {
   return (
-    <div>
-        
-       <h1>Su_di_me</h1> 
-        
+    <div className="bg-home d-flex flex-column justify-content-center align-items-center text-white" style={{
+      backgroundImage: "url('/img/lupo2.jpg')",
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      height: '100vh'
+    }}>
+    
+      <div className="position-absolute top-0 w-100 d-flex justify-content-center p-3">
+  
+      </div>
+
+      <div className="text-center px4"> 
+        <p>
+          Tecnicamente, la devanāgarī è un alfasillabario: sebbene possano anche apparire separate, 
+          consonanti e vocali vengono di regola congiunte in legature grafiche di cui l'unità fondamentale
+          non è il singola fonema, bensì la sillaba akṣara.
+          Questa è sempre intesa come accumulo di una o più consonanti terminante
+          in una vocale o in un dittongo.
+          Al contrario dell'alfabeto latino, in cui consonanti e vocali seguono 
+          una successione casuale, l'alfasillabario devanāgarī dispone le lettere
+          secondo precisi criteri fonologici. Inizia con le vocali, seguono 
+          i dittonghi, quindi le consonanti. Ogni gruppo è a sua volta ordinato 
+          in cinque «posizioni» , secondo la successione del punto di articolazione, dalla gola alle labbra:
+        </p>
+      </div>
     </div>
   )
 }
